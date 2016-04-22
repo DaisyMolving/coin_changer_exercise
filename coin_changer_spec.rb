dae8a6a9d979d4e95b1ff23b774b875d7d4e923c
Daisy Molving <daisymolving@gmail.com>
2016-04-22 10:47:15 +0100

    refactored tests to use let block, all pass

--- coin_changer_spec.rb
+++ coin_changer_spec.rb
@@ -1,20 +1,20 @@
 require_relative './coin_changer'
 
 describe CoinChanger do
+
+let(:coin_change) { CoinChanger.new }
+
   it 'takes an amount of 0 and returns no coins' do
-	 coin_change = CoinChanger.new
 	 result = coin_change.give_coins(0)
 	 expect(result).to eq []
   end
   
   it 'takes an amount of 1 and returns 1 coin' do
-	 coin_change = CoinChanger.new
 	 result = coin_change.give_coins(1)
 	 expect(result).to eq [1]
   end
 
   it 'takes an amount of 5 and returns a 5 coin' do
-	 coin_change = CoinChanger.new
 	 result = coin_change.give_coins(5)
 	 expect(result).to eq [5]
   end
