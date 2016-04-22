b9be71782cb52dd3cdc40d5f2068bd0250e18059
Daisy Molving <daisymolving@gmail.com>
2016-04-22 10:24:18 +0100

    wrote test for amount of 5 to give one coin of 5. test fails

--- coin_changer_spec.rb
+++ coin_changer_spec.rb
@@ -12,4 +12,10 @@ describe CoinChanger do
 	 result = coin_change.give_coins(1)
 	 expect(result).to eq [1]
   end
+
+  it 'takes an amount of 5 and returns a 5 coin' do
+	 coin_change = CoinChanger.new
+	 result = coin_change.give_coins(5)
+	 expect(result).to eq [5]
+  end
 end
