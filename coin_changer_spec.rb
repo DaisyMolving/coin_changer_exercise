e1d1a85f6e062119225ac183012e0fac02ee1d38
Daisy Molving <daisymolving@gmail.com>
2016-04-22 09:49:29 +0100

    wrote first test to check that if amount of 0 given to change that no coins are returned. test fails

---
+++ coin_changer_spec.rb
@@ -0,0 +1,7 @@
+describe CoinChanger do
+  it 'takes an amount of 0 and returns no coins' do
+	 coin_change = CoinChanger.new
+	 result = coin_change.give_coins(0)
+	 expect(result).to eq []
+  end
+end
